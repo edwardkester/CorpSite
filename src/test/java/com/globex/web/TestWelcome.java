74d02029284d62c67c567268a85abb1e14cb17ad
Edward [Ed] Kester <esk.boc@gmail.com>
2019-08-09 10:18:08 -0400

    STRY0010005: Add 2nd Test
    Add 2nd test to src/test/java/com/globex/web/TestWelcome.java
    
        @Test
        public void testAbc2() {
            assertEquals(4, 2 + 2);
        }

--- src/test/java/com/globex/web/TestWelcome.java
+++ src/test/java/com/globex/web/TestWelcome.java
@@ -51,5 +51,11 @@ public class TestWelcome {
     public void testAbc() {
         assertEquals(2, 1 + 1);
     }
+    
+    
+    @Test
+    public void testAbc2() {
+        assertEquals(4, 2 + 2);
+    }
 
 }
